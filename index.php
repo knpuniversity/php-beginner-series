295758d62ce2b195bfb0e5bc8e772ffc9f0d33d5
Ryan Weaver <ryan@thatsquality.com>
2013-09-27 13:32:19 -0500

    Post ch5 (array3)!

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+    $pet1 = array(
+        'name' => 'Chew Barka',
+        'breed' => 'Bichon',
+        'age'  => '2 years',
+        'weight' => 8,
+        'bio'   => 'The park, The pool or the Playground - I love to go anywhere! I am really great at... SQUIRREL!',
+        'filename' => 'pet1.png'
+    );
+
+    $pet2 = array(
+        'name' => 'Spark Pug',
+        'breed' => 'Pug',
+        'age'  => '1.5 years',
+        'weight' => 11,
+        'bio'   => 'You want to go to the dog park in style? Then I am your pug!',
+        'filename' => 'pet2.png'
+    );
+
+    $pet3 = array(
+        'name' => 'Pico de Gato',
+        'breed' => 'Bengal',
+        'age'  => '5 years',
+        'weight' => 9,
+        'bio'   => 'Oh hai, if you do not have a can of salmon I am not interested.',
+        'filename' => 'pet3.png'
+    );
+
+    $pancake = array(
+        'name' => 'Pancake',
+        'age' => '1 year',
+        'weight' => 9,
+        'bio' => 'Treats and Snoozin!',
+        'filename' => 'pancake.png'
+    );
+    $pancake['breed'] = 'Bulldog';
+
+    $pets = array($pet1, $pet2, $pet3, $pancake);
+    $pets = array_reverse($pets);
+
+    $cleverWelcomeMessage = 'All the love, none of the crap!';
+    $pupCount = count($pets);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,11 +109,6 @@
 
     <div class="jumbotron">
         <div class="container">
-            <?php
-                $cleverWelcomeMessage = 'All the love, none of the crap!';
-                $pupCount = rand(50, 100);
-            ?>
-
             <h1><?php echo strtoupper(strtolower($cleverWelcomeMessage)); ?></h1>
 
             <p>Over <?php echo $pupCount ?> pet friends!</p>
@@ -78,54 +117,25 @@
         </div>
     </div>
 
-    <?php
-        $pet1 = 'Chew Barka';
-        $pet2 = 'Spark Pug';
-        $pet3 = 'Pico de Gato';
-
-        $pancake = array(
-            'name' => 'Pancake',
-            'age' => '1 year',
-            'weight' => 9,
-            'bio' => 'Treats and Snoozin!',
-            'filename' => 'pancake.png'
-        );
-        $pancake['breed'] = 'Bulldog';
-
-        $pets = array($pet1, $pet2, $pet3);
-        $pets[] = 'Kitty Gaga';
-    ?>
-
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 pet-list-item">
-                <h2><?php echo $pancake['name']; ?></h2>
+            <?php foreach ($pets as $cutePet) { ?>
+                <div class="col-md-4 pet-list-item">
+                    <h2><?php echo $cutePet['name']; ?></h2>
 
-                <img src="/images/<?php echo $pancake['filename']; ?>" class="img-rounded" />
+                    <img src="/images/<?php echo $cutePet['filename']; ?>" class="img-rounded" />
 
-                <blockquote class="pet-details">
-                    <span class="label label-info"><?php echo $pancake['breed']; ?></span>
-                    <?php echo $pancake['age']; ?>
-                    <?php echo $pancake['weight']; ?> lbs
-                </blockquote>
+                    <blockquote class="pet-details">
+                        <span class="label label-info"><?php echo $cutePet['breed']; ?></span>
+                        <?php echo $cutePet['age']; ?>
+                        <?php echo $cutePet['weight']; ?> lbs
+                    </blockquote>
 
-                <p>
-                    <?php echo $pancake['bio']; ?>
-                </p>
-            </div>
-
-            <?php
-                foreach ($pets as $cutePet) {
-                    echo '<div class="col-lg-4">';
-                    echo '<h2>';
-                    echo $cutePet;
-                    echo '</h2>';
-                    echo '<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                                        condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                                        euismod. Donec sed odio dui. </p>';
-                    echo '</div>';
-                }
-            ?>
+                    <p>
+                        <?php echo $cutePet['bio']; ?>
+                    </p>
+                </div>
+            <?php } ?>
         </div>
 
         <hr>
