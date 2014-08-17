eaef3f2858aa0f4c4fd399f8a8f61f8dd0718af8
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:39:01 -0400

    [tuts-apply-diff] Applying existing diff file for step layout-create-and-use-header-footer

--- contact.php
+++ contact.php
@@ -1,7 +1,10 @@
 <?php
 require 'lib/functions.php';
 ?>
+<?php require 'layout/header.php'; ?>
 
 <h1>
     Helping you find your new best friend from over <?php echo count(get_pets()); ?> pets.
 </h1>
+
+<?php require 'layout/footer.php'; ?>
