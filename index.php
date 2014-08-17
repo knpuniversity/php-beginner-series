9b168540c3c9bd0f3a9632e572f98ff04be672e4
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:38:52 -0400

    [tuts-apply-diff] Applying existing diff file for step functions-ucwords

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
     <div class="jumbotron">
         <div class="container">
             <?php
-                $cleverWelcomeMessage = 'All the love, none of the crap!';
+                $cleverWelcomeMessage = ucwords('All the love, none of the crap!');
                 $pupCount = rand(50, 100);
             ?>
 
