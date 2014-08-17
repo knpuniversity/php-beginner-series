3e8224b0fd022aa834c4fe2d1531a372be241ad3
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:38:52 -0400

    [tuts-apply-diff] Applying existing diff file for step functions-misisng-arg-error

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
     <div class="jumbotron">
         <div class="container">
             <?php
-                $cleverWelcomeMessage = ucwords('All the love, none of the crap!');
+                $cleverWelcomeMessage = ucwords();
                 $pupCount = rand(50, 100);
             ?>
 
