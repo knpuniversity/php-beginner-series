f12f2e189eaf0a037437ab488a188b7042422cd2
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:38:59 -0400

    [tuts-apply-diff] Applying existing diff file for step if-fix-syntax

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
                         <?php
                         if (!array_key_exists('age', $cutePet) || $cutePet['age'] == '') {
                             echo 'Unknown';
-                        } elseif ($cutePet['age'] == 'hidden')
+                        } elseif ($cutePet['age'] == 'hidden') {
                             echo '(contact owner for age)';
                         } else {
                             echo $cutePet['age'];
