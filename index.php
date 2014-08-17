3a47a76dcd94d2e286a5f0ad500c84a7e044f08a
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:38:53 -0400

    [tuts-apply-diff] Applying existing diff file for step loops-first-foreach

--- index.php
+++ index.php
@@ -88,33 +88,18 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
-                <h2><?php echo $pet1; ?></h2>
-
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                    euismod. Donec sed odio dui. </p>
-
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2><?php echo $pet2; ?></h2>
-
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-                    euismod. Donec sed odio dui. </p>
-
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2><?php echo $pet3; ?></h2>
-
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
-                    porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-                    fermentum massa justo sit amet risus.</p>
-
-                <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-            </div>
+            <?php
+                foreach ($pets as $pet) {
+                    echo '<div class="col-lg-4">';
+                    echo '<h2>';
+                    echo $pet;
+                    echo '</h2>';
+                    echo '<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                                        condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+                                        euismod. Donec sed odio dui. </p>';
+                    echo '</div>';
+                }
+            ?>
         </div>
 
         <hr>
