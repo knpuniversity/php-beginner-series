66bb82a3284b7448b1d0c28c701b311bc017f405
Ryan Weaver <ryan@thatsquality.com>
2014-08-17 15:38:52 -0400

    [tuts-apply-diff] Applying existing diff file for step functions-rand-2-arguments

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
         <div class="container">
             <?php
                 $cleverWelcomeMessage = 'All the love, none of the crap!';
-                $pupCount = rand(50);
+                $pupCount = rand(50, 100);
             ?>
 
             <h1><?php echo $cleverWelcomeMessage; ?></h1>
